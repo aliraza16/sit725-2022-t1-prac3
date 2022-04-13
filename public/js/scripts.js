e8952fc1965ea0173edd18a7959f5d052a9bc16b
aliraza16 <63829069+aliraza16@users.noreply.github.com>
2022-04-14 02:17:33 +0500

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -21,7 +21,31 @@ const cardList = [
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
 
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+        
+        
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+     
 const submitForm = () => {
     let formData = {};
     formData.first_name = $('#first_name').val();
